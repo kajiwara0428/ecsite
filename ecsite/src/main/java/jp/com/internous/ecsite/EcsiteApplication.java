e2ae55cfdc08b1c84825c289477417cb90b7c94c
kajiwara0428 <fuyuko.0428@gmail.com>
2021-07-26 12:05:55 +0900

    ログインECサイト

---
+++ ecsite/src/main/java/jp/com/internous/ecsite/EcsiteApplication.java
@@ -0,0 +1,13 @@
+package jp.com.internous.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteApplication.class, args);
+	}
+
+}
---
+++ ecsite/src/main/java/jp/com/internous/ecsite/model/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package jp.com.internous.ecsite.model.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.com.internous.ecsite.model.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	List<User> findByUserNameAndPassword(String userName,String password);
+
+}
